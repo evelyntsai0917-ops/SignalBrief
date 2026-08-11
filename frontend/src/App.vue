dbbc5828c692a2f4296dc3deb59dc58eb3e9f3f3
peijung tsai <ptsa0008@student.monash.edu>
2026-08-11 21:07:25 +0800

    Connect frontend to backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8000/')
+  const data = await response.json()
+  message.value = data.message
+})
+</script>
+
 <template>
   <h1>SignalBrief</h1>
+  <p>{{ message }}</p>
 </template>
